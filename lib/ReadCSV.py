fea20ea6ed91977de4939de1014d34eb0acc202e
ROUGER Nicolas <nicolas.rouger@laplace.univ-tlse.fr>
2024-12-20 16:41:07 +0100

    Updated ReadCSV.py

--- lib/ReadCSV.py
+++ lib/ReadCSV.py
@@ -4,9 +4,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def ReadXYfromcsv(Name):
+    yvoltage = np.genfromtxt(Name+'valueY.csv', delimiter=',')
+    xtime = np.genfromtxt(Name+'timeX.csv', delimiter=',')
+    print(f"Loaded {len(xtime)}, {len(yvoltage)} points")
+    return xtime,yvoltage
+    
 # Load data
-yvoltage = np.genfromtxt('csv-C4-5-Test3-valueY.csv', delimiter=',')
-xtime = np.genfromtxt('csv-C4-5-Test3-timeX.csv', delimiter=',')
-print(f"Loaded {len(xtime)}, {len(yvoltage)} points")
-plt.plot(xtime,yvoltage)
+time1,C1=ReadXYfromcsv('../data/csv-C1-1-')
+plt.figure(1)
+plt.plot(time1,C1)
+
+time2,C2=ReadXYfromcsv('../data/csv-C2-1-')
+plt.figure(2)
+plt.plot(time2,C2,color='green')
+
+time3,C3=ReadXYfromcsv('../data/csv-C3-1-')
+plt.figure(3)
+plt.plot(time3,C3,color='red')
+
+time4,C4=ReadXYfromcsv('../data/csv-C4-1-')
+plt.figure(4)
+plt.plot(time4,C4)
+
 plt.show()
