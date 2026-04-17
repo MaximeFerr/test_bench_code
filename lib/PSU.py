ae61b01eb36f2775eb95b16868cdf96018f6c8fb
MaximeFerr <ferrand.maxime.fr14@gmail.com>
2026-04-17 16:33:10 +0200

    Clean the project and add lib pandas in requierment file

--- lib/PSU.py
+++
@@ -1,9 +0,0 @@
-import time
-import pyvisa
-from .Instrument import Instrument
-
-class PSU(Instrument):
-	def __init__(self, rm, instr):
-		super().__init__(rm, instr)
-		self.channel = None
-		# print("PSU")
--- lib/plot_res.py
+++
@@ -1,8 +0,0 @@
-#%% 
-import matplotlib.pyplot as plt
-import pandas as pd
-
-df = pd.read_csv('res_2024.csv')
-
-#%%
-plt.plot(df['voltage'])
